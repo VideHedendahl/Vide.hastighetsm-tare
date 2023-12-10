115bc97be77f52f13ddafcf6531932c106699e0f
VideHedendahl <vide.hedendahl@elev.ga.ntig.se>
2023-12-10 15:41:39 +0100

    Update h_o_m.ino
    Added comments to code

--- h_o_m.ino
+++ h_o_m.ino
@@ -1,20 +1,28 @@
+
 /*
-   created by Rui Santos, https://randomnerdtutorials.com
+ * Jag har skapat hastighetsmätare som kan mäta hastigheten som ett 
+ * objekt rör sig i. Hastighetsmätaren kan sätaspå och stengasav. Man 
+ * kan också ändra på vilket "mode" som den är insteld på. De första 
+ * läget skrivr ut hastigheten i realtid medans de andra läget skriver
+ * ut de största värdet som den har fåtfram. Vär man stengerav seter på 
+ * ändrar läge så spelas de också up olika djud som indikerar ens
+ * handling. De skrivs även ut en text på skärmen som beskriver ens 
+ * ändring. Detta är för att göra hastighetsmätaren mer lätt användlig.
+ */
 
-   Complete Guide for Ultrasonic Sensor HC-SR04
+ 
+/*
+ * Här includerar jag alla biblotek som används och raden underskriver
+ * jag in info om vilken oledskärm de är jag använder till daton.
+ */ 
+#include <U8glib.h> 
 
-    Ultrasonic sensor Pins:
-        VCC: +5VDC
-        Trig : Trigger (INPUT) - Pin11
-        Echo: Echo (OUTPUT) - Pin 12
-        GND: GND
-*/
-#include <U8glib.h>
+U8GLIB_SSD1306_128X64 oled(U8G_I2C_OPT_NO_ACK); 
 
-U8GLIB_SSD1306_128X64 oled(U8G_I2C_OPT_NO_ACK);
+//här initsierar jag alla variabler som används
 
-int trigPin = 11;    // Trigger
-int echoPin = 12;    // Echo
+int trigPin = 11;    
+int echoPin = 12;    
 
 float cm1;
 float cm2;
@@ -26,11 +34,13 @@ int toggleMode = 0;
 
 int speakerPin = 8;
 
+//I void setup beskriver man för arduinot vilka pins som ska användas och åt
+//vad, om den ska ta in datta(input) eller geut(output). I setup intruduserar
+//jag också vlken font som oled skärmen ska visa.
+
 void setup() {
   oled.setFont(u8g_font_helvB10);
-  //Serial Port begin
   Serial.begin (9600);
-  //Define inputs and outputs
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(2,INPUT);
@@ -40,9 +50,18 @@ void setup() {
 
 void loop() {
 
-  if ( OnOffSwitch() == 1) {
-    if ( ModeSwitch() == 1){
-      
+  if ( OnOffSwitch() == 1) { // här sktiver jag att om onoffswitch ger värde 1 
+                             // så ska pogramet sätas på annars hender ingenting.
+    if ( ModeSwitch() == 1){ // här sktiver jag att om ModeSwitch är lika med 1 så ska pogramet för
+                             // maxhastighet vara på men om värdet är något annat så ska pogramet för
+                             //hastighet varapå.
+/*
+ * i denär delen använder jag funktionen "hastighet" för att tareda 
+ * på hastigheten i stunden. Sedan jämför jag mitt nya värde med 
+ * största värdet jag har fåt in. Om mitt nya värde är störe än 
+ * mitt föra kommer den att ersäta och printa ut de nya värdet på 
+ * skermen.
+ */
       hastighetms = hastighet();
 
       if (abs(hastighetms) > abs(maxhastighet)) {
@@ -57,7 +76,10 @@ void loop() {
   
     }
     else {
-      
+/*
+ * I denhär delen ber jag funktionen "hastighet" att ge mig des värde
+ * nu i stnden och sedan printar den ut de på skärmen.
+ */
       hastighetms = hastighet();
       
       Serial.print(hastighetms);
@@ -72,6 +94,18 @@ void loop() {
   }
 }
 
+/*
+ *funktionen nedan (OnOffSwitch) börjar med att kolla om den har fått
+ *en singnal. Om den har fått en singnal så så kollar den vilket värde
+ *som variablen (toggleOnOff) har (antingen 1 eller 0). Om variablen 
+ *har värdet 1 så kommer den slåöver värdet till 0 och samma sak tvärt
+ *om. Om funktionen slåröver från 1 till 0 så updaterar den oledsärmen
+ *till att det står off och ändrar även variablen toggleMode till 0
+ *detta gör så att hastighetsmätaren alltid är på ett ovh samma läge 
+ *när den säts på. Det spelar också en viss melodi när den går till
+ *off. När toggleOnOff går från 0 till 1 spellas det samma melodi fast 
+ *baklänges. 
+ */
 int OnOffSwitch() {
   if (digitalRead(2)== 1){
     if (toggleOnOff == 1){
@@ -108,7 +142,7 @@ int OnOffSwitch() {
   }
   return toggleOnOff;
 }
-
+// funktionen ModeSwitch aggerar väldigt likt OnOffSwitch.
 int ModeSwitch(){
   if (digitalRead(4)== 1) {
     if (toggleMode == 1) {
@@ -128,7 +162,14 @@ int ModeSwitch(){
   }
   return toggleMode;
 }
-
+/*
+ * funktionen hastighet tar in två stycken värden från fuktonen avstand
+ * och gör om värdena till två variabler (cm1 , cm2). dett fins ett 
+ * delay mellan cm 1 och cm2 på 200 millisekunder. Sän tar daton och 
+ * delar skilnaden i streka på tiden och får dör med fram hastigheten.
+ * göra systemet mer nogrant så att den kan mäta högre och lägre hastigheter 
+ * bätre så kan man öka eller minska värdet på delay.
+ */
 float hastighet() {
   
   cm1 = avstand();
@@ -137,7 +178,14 @@ float hastighet() {
 
   return ((cm1 - cm2) / 100.0) / 0.2;
 }
-
+/*
+ * funktionen avstand mäter avståndet mellan ultradjudsensorn och 
+ * nermaste objekt. Detta gör den genom att den ber trigpin att skika
+ * ut en singnal. Sedan mäter ecoPin hur lång tid det tar för 
+ * singnalen att komma tilbaks. Sen delar man tiden med två i och med
+ * att ljudet åker både fram och tilbaks. Slutligen delar man de med 
+ * ljudets hastighet för att få avståndet.
+ */
 long avstand() {
   long duration;
   digitalWrite(trigPin, LOW);
@@ -149,9 +197,9 @@ long avstand() {
   pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
  
-  return (duration/2) * 0.0343;     // Divide by 29.1 or multiply by 0.0343
+  return (duration/2) * 0.0343;  
 }
-
+// Den här funktionen updaterar särmen och bestemer var texten ska skrivas.
 void updateOled(String text){
   oled.firstPage();
   do{
